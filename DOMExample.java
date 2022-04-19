e45df4087e83f699162327e13fe3a8e16a1902b6
Akaed <43893798+Akaed@users.noreply.github.com>
2022-04-20 01:51:52 +0300

    Add files via upload

---
+++ DOMExample.java
@@ -0,0 +1,30 @@
+import jdk.internal.org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DOMExample {
+
+    // Список для сотрудников из XML файла
+    private static ArrayList<Employee> employees = new ArrayList<>();
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, org.xml.sax.SAXException {
+        // Получение фабрики, чтобы после получить билдер документов.
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        // Получили из фабрики билдер, который парсит XML, создает структуру Document в виде иерархического дерева.
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // Запарсили XML, создав структуру Document. Теперь у нас есть доступ ко всем элементам, каким нам нужно.
+        Document document = builder.parse(new File("src/test.xml"));
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,20 @@
+
+
+
+public class Employee {
+    private String name, job;
+
+    public Employee(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+}
